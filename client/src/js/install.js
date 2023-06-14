8969c986c620f34fbe78103cd2534f150f308207
Brandon Guerrero <brandon_g909@yahoo.com>
2023-06-13 19:59:52 -0700

    starter code

---
+++ client/src/js/install.js
@@ -0,0 +1,5 @@
+const butInstall = document.getElementById('buttonInstall');
+const textHeader = document.getElementById('textHeader');
+
+// Logic for installing the PWA
+// TODO: Add an event handler to the `beforeinstallprompt` event
